81ad7e703d9a132ae98488ade1bb591a2db2b02b
Raimundo <raimundoneto.rn17@gmail.com>
2020-05-22 19:08:08 -0400

    Creating a first test, sum test 1 + 4 should be 5

--- src/calculator.service.test.js
+++ src/calculator.service.test.js
@@ -5,4 +5,10 @@ import CalculatorService from './calculator.service'
 
 describe('CalculatorService test', () => {
     const [calculate] = CalculatorService()
+
+    it('should garanteed with 1 + 4 = 5', () => {
+        const sum = calculate(1, 4, '+')
+
+        expect(sum).toEqual(5)
+    })
 })

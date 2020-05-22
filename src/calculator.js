c65c04e8687ad2d100d49f3c37b831f5c412abd4
Raimundo <raimundoneto.rn17@gmail.com>
2020-05-22 18:03:04 -0400

    Adding all buttons for simple calcultor

--- src/calculator.js
+++ src/calculator.js
@@ -27,6 +27,66 @@ function Calculator() {
             readOnly='readOnly'/>
           </Col>
         </Row>
+
+        <Row>
+          <Col xs='3'>
+            <Button variant='light'>7</Button>
+          </Col>
+          <Col xs='3'>
+            <Button variant='light'>8</Button>
+          </Col>
+          <Col xs='3'>
+            <Button variant='light'>9</Button>
+          </Col>
+          <Col xs='3'>
+            <Button variant='warning'>/</Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs='3'>
+            <Button variant='light'>4</Button>
+          </Col>
+          <Col xs='3'>
+            <Button variant='light'>5</Button>
+          </Col>
+          <Col xs='3'>
+            <Button variant='light'>6</Button>
+          </Col>
+          <Col xs='3'>
+            <Button variant='warning'>*</Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs='3'>
+            <Button variant='light'>1</Button>
+          </Col>
+          <Col xs='3'>
+            <Button variant='light'>2</Button>
+          </Col>
+          <Col xs='3'>
+            <Button variant='light'>3</Button>
+          </Col>
+          <Col xs='3'>
+            <Button variant='warning'>-</Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs='3'>
+            <Button variant='light'>.</Button>
+          </Col>
+          <Col xs='3'>
+            <Button variant='light'>0</Button>
+          </Col>
+          <Col xs='3'>
+            <Button variant='success'>=</Button>
+          </Col>
+          <Col xs='3'>
+            <Button variant='warning'>+</Button>
+          </Col>
+        </Row>
       </Container>
     </Jumbotron>
   );

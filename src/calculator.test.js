6e02fbc3a57d8fb09571a07ae01d479e05d16698
Raimundo <raimundoneto.rn17@gmail.com>
2020-05-22 21:15:08 -0400

    Deleting unnecessary files: calculatorTest

--- src/calculator.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Calculator from './calculator'
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Calculator />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});

72d317bd65672a9d836c92ba1a51aea4b31d91eb
Raimundo <raimundoneto.rn17@gmail.com>
2020-05-22 21:17:42 -0400

    Adding a switch/case to select operation and execute these

--- src/calculator.service.js
+++ src/calculator.service.js
@@ -1,5 +1,11 @@
 function CalculatorService() {
     function calculate(a, b, operation){
+        switch (operation) {
+            case '+' :
+                return a + b 
+            case '-' :
+                return a - b
+        }
         return a + b
     }
 
